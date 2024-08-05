3626b800e345f00d3ee2f4e3ef35408c089ddd66
ArpanSaha404 <arpan50saha@gmail.com>
2024-08-05 18:51:00 +0530

    conn Fixed

--- conn.js
+++ conn.js
@@ -1,17 +1,70 @@
-const mongo = require('mongoose');
+const mongoose = require('mongoose');
+// const mongo = require('mongoose');
 
-const conn = async (req , res) => {
+
+let uri = "mongodb+srv://arpan50saha:todo%401@tododbc.eeuugu2.mongodb.net/newDB?retryWrites=true&w=majority&appName=todoDBC";
+
+
+// async function conn() {
+//     try {
+//         await client.connect().then(() => {
+//             console.log('DB Connected...');
+//         });
+//     }
+//     catch(error)
+//     {
+//         throw error;
+//     }
+// };
+// conn();
+
+// async function conn() {
+//     try {
+//         await client.connect();
+
+//         const database = client.db("todoDB");
+//         const col = database.collection("todoDB");
+
+//         const cursor = col.find();
+//         await cursor.forEach(doc => console.dir(doc));
+//         } finally {
+//         await client.close();
+//     }
+// };
+// conn().catch(console.dir);
+
+
+
+
+
+// const conn = async () => {
+//     try {
+//         await mongo
+//             .connect("mongodb+srv://arpan50saha:todo%4011@tododbc.eeuugu2.mongodb.net/")
+//             .then(() => {
+//                 console.log("DB Connected...");
+//             });
+//     } catch (error) {
+//         res.status(400).json({
+//             message : "Not Connected...Some Error..."
+//         });
+//         // throw error;
+//     }
+// };
+
+// conn();
+
+const conn = async () => {
     try {
-        await mongo
-            .connect("mongodb://localhost:27017/TodoApp")
+        await mongoose
+            .connect(uri)
             .then(() => {
-                console.log("DB Connected...");
+                console.log("DB Connected...")
             });
-    } catch (error) {
-        res.status(400).json({
-            message : "Not Connected...Some Error..."
-        });
+    }
+    catch(error)
+    {
+        console.log("DB Not Connected!!!");
     }
 };
-
 conn();
